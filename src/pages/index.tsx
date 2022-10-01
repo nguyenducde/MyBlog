830daad2da66b67ba7fdf38c7087b74b122a9d65
unknown <quang.luong2@executionlab.asia>
2022-10-01 20:19:04 +0700

    UI: Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import NavBar from '../components/NavBar'
 
 const Home: NextPage = () => {
   return (
     <main>
-      <h1>AAA</h1>
+      <NavBar />
+      <h1 style={{ position: 'absolute', width: '75%', top: 0, right: 0 }}>AAAAAAAAAA</h1>
     </main>
   )
 }
