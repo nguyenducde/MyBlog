c0941b712439af33e87249696eefb440ba6e6365
luongtuyenquang <luongtuyenquang77@gmail.com>
2022-10-17 23:20:55 +0700

    Fix link google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ const Document: React.FC = () => {
     <Html>
       <Head>
         <link rel='stylesheet' href='https://pro.fontawesome.com/releases/v5.2.0/css/all.css' />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap'
+        />
       </Head>
       <body>
         <Main />
