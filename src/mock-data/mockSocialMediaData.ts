7386411787fff4b380cee9fb1ed9e538ecf44c95
nguyenducde <ducde678@gmail.com>
2023-04-13 16:25:00 +0700

    fix info

--- src/mock-data/mockSocialMediaData.ts
+++ src/mock-data/mockSocialMediaData.ts
@@ -3,25 +3,25 @@ const mockSocialMediaData = [
     id: 1,
     name: 'facebook',
     icon: 'fab fa-facebook-f',
-    link: 'https://www.facebook.com/fb.luongtuyenquang',
+    link: 'https://www.facebook.com/nguyenducde.tt',
   },
   {
     id: 2,
     name: 'linkedin',
     icon: 'fab fa-linkedin-in',
-    link: 'https://www.linkedin.com/in/luong-tuyen-quang/',
+    link: 'https://www.linkedin.com/in/nguyenducde/',
   },
   {
     id: 3,
     name: 'github',
     icon: 'fab fa-github',
-    link: 'https://github.com/luongtuyenquang',
+    link: 'https://github.com/nguyenducde',
   },
   {
     id: 4,
     name: 'gmail',
     icon: 'far fa-envelope',
-    link: 'luongtuyenquang.it@gmail.com',
+    link: 'ducdecoder@gmail.com',
   },
 ]
 
